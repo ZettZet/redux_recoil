aa74b924edbeae1e612f021b88824f4a12e90d89
ferahar <aklian@ya.ru>
2022-12-08 03:01:14 +0500

    add equalityFn

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -4,8 +4,14 @@ import { useAppSelector } from './common/hooks'
 import { getElements } from './store/reducers/elementsSlice'
 import { Element, Info, TopPanel, Viewer } from './components'
 
+import { ElementState } from './types'
+
+const equalityFn = (a: ElementState[], b: ElementState[]) => {
+	return b.every((el, index) => el.id === (a[index] && a[index].id));
+}
+
 const App: FC = () => {
-	const elements = useAppSelector(getElements)
+	const elements = useAppSelector(getElements, equalityFn)
 	return (
 		<>
 			<TopPanel />
