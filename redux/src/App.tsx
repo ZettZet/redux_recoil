8084288cf41979fa7f4c75d17b627b0d8330650a
ferahar <69187061+ferahar@users.noreply.github.com>
2022-12-12 20:53:12 +0500

    add equalityFn (#1)
    * add equalityFn
    
    * simplify_callback

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -4,8 +4,13 @@ import { useAppSelector } from './common/hooks'
 import { getElements } from './store/reducers/elementsSlice'
 import { Element, Info, TopPanel, Viewer } from './components'
 
+const equalityFn = (state: unknown[], stateNew: unknown[]) => {
+	// простая проверка
+	return state.length === stateNew.length;
+}
+
 const App: FC = () => {
-	const elements = useAppSelector(getElements)
+	const elements = useAppSelector(getElements, equalityFn)
 	return (
 		<>
 			<TopPanel />
